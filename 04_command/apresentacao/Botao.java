98a1f272dce347cdca7507cd1992645f940d0967
Roberto Silva <2024018962@aluno.riogrande.ifrs.edu.br>
2026-03-25 23:32:12 -0300

    Add files via upload

--- 04_command/apresentacao/Botao.java
+++ 04_command/apresentacao/Botao.java
@@ -1,1 +1,15 @@
+package apresentacao;
 
+import negocio.Comando;
+
+
+public class Botao {
+    private Comando comando;
+    public void setComando(Comando c) { 
+        this.comando = c; 
+    }
+    
+    public void apertar() {
+        comando.executar();
+    }
+}
---
+++ 04_command/apresentacao/Main.java
@@ -0,0 +1,35 @@
+package apresentacao;
+
+import negocio.Comando;
+import negocio.ComandoColar;
+import negocio.ComandoCopiar;
+import negocio.Editor;
+
+
+public class Main {
+    public static void main(String[] args) {
+       
+        Editor meuEditor = new Editor();
+
+        
+        Comando acaoCopiar = new ComandoCopiar(meuEditor);
+        Comando acaoColar = new ComandoColar(meuEditor);
+
+        
+        Botao botaoCopiar = new Botao();
+        Botao botaoColar = new Botao();
+
+        botaoCopiar.setComando(acaoCopiar);
+        botaoColar.setComando(acaoColar);
+
+        System.out.println("--- Usuário clica no botão de copiar ---");
+        botaoCopiar.apertar();
+
+        System.out.println("\n--- Usuário clica no botão de colar ---");
+        botaoColar.apertar();
+        
+        System.out.println("\n--- Trocando a função do botão de copiar para colar ---");
+        botaoCopiar.setComando(acaoColar);
+        botaoCopiar.apertar();
+    }
+}
