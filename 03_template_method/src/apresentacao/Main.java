72ee4c49a03dfb82037310262c78d848a47e6e43
Roberto Silva <2024018962@aluno.riogrande.ifrs.edu.br>
2026-03-25 23:06:47 -0300

    Add files via upload

--- 03_template_method/src/apresentacao/Main.java
+++ 03_template_method/src/apresentacao/Main.java
@@ -1,1 +1,25 @@
+package apresentacao;
 
+import negocio.Npk;
+import negocio.Supersimples;
+
+
+
+
+public class Main {
+
+    public static void main(String[] args) {
+        
+        
+        Npk npk1 = new Npk();
+        npk1.produzir();
+
+
+        Supersimples super1 = new Supersimples();
+        super1.produzir();
+
+
+
+    }
+
+}
